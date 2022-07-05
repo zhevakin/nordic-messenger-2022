7b7ad476585d7e4910a0f8d70798ca48412c21b0
Ivan Zhevakin <ixvakin@gmail.com>
2022-07-05 21:29:28 +0300

    message sending

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import chatsSlice from '../features/chats/chatsSlice'
 import messagesSlice from '../features/chats/messagesSlice'
+import chatMiddleware from '../features/chats/chatMiddleware'
 
 export const store = configureStore({
   reducer: {
     chats: chatsSlice,
     messages: messagesSlice,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(chatMiddleware)
   },
 })
