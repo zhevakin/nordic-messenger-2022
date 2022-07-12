639091b03e931e33730cf91c4ac5050a092273e6
Ivan Zhevakin <ixvakin@gmail.com>
2022-07-12 20:08:02 +0300

    improved location components

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
+
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -5,22 +9,41 @@ const options = {
 }
 
 function Location({ onLocation }) {
+  const [loading, setLoading] = useState(false)
+  const [value, setValue] = useState()
+  const [error, setError] = useState(false)
+
   const onSuccess = (pos) => {
+    setLoading(false)
+    setValue(pos.coords)
     onLocation(pos.coords)
   }
 
   const onError = (err) => {
+    setLoading(false)
+    setError(true)
     console.log(err)
   }
 
   const handleClick = () => {
+    setLoading(true)
     navigator.geolocation.getCurrentPosition(onSuccess, onError, options)
   }
 
   return (
-    <button type="button" onClick={handleClick}>
+    <Button variant="outline-primary" type="button" onClick={handleClick}>
       Определить координаты
-    </button>
+      {loading && (
+        <Spinner
+          className="ms-2"
+          size="sm"
+          animation="border"
+          variant="primary"
+        />
+      )}
+      {value && <span className="ms-2">✅</span>}
+      {error && <span className="ms-2">❌</span>}
+    </Button>
   )
 }
 
